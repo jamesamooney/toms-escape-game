0ffb1d9b18badab8ede85c63eac851ca00407cc9
James Mooney <jmooney9304@gmail.com>
2022-05-03 14:58:14 +0100

    Move on to level 2 when level 1 completed
    Co-authored-by: Phil Bligh <PhilBligh@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 // import logo from './logo.svg';
-import { Key } from './components/Key'
-import { Door } from './components/Door'
-import { Padlock } from './components/Padlock'
-import { Book } from './components/Book'
-import { BookClue } from './components/BookClue'
-import { PadlockForm } from './components/PadlockForm'
+
 import { useState } from 'react'
+import { LevelOne } from './components/LevelOne'
+import { LevelTwo } from './components/LevelTwo'
 
 import './App.css';
 
 function App() {
-
-  const [padlockClicked, setPadlockClicked] = useState(false)
-  const [showBookClue, setShowBookClue] = useState(false)
-  const [passwordCorrect, setPasswordCorrect] = useState(false)
-  const [doorOneOpen, setDoorOneOpen] = useState(false)
-  const [hasKeyOne, setHasKeyOne] = useState(false)
+  const [levelOneComplete, setLevelOneComplete] = useState(false)
 
   return (
-    <div className="App">
-        {passwordCorrect && <Key setHasKeyOne={setHasKeyOne}/>}
-        <Door setDoorOneOpen={setDoorOneOpen} doorOneOpen={doorOneOpen} hasKeyOne={hasKeyOne}/>
-        <Book bookClick={() => setShowBookClue(!showBookClue)} />
-        {showBookClue && <BookClue />}
-        <Padlock padClick={() => setPadlockClicked(!padlockClicked)}/>
-        {padlockClicked && <PadlockForm setPasswordCorrect={setPasswordCorrect} />}
+    <div>
+      {!levelOneComplete && <LevelOne setLevelOneComplete={setLevelOneComplete}/>}
+      {levelOneComplete && <LevelTwo />}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Door.js
+++ src/components/Door.js
@@ -2,16 +2,24 @@ import doorOneClosedImg from "../images/items/closedDoor.png"
 import doorOneOpenImg from  "../images/items/openDoor.png"
 
 
-export const Door = ({ setDoorOneOpen, hasKeyOne, doorOneOpen }) => {
+export const Door = ({ setDoorOneOpen, hasKeyOne, doorOneOpen, setLevelOneComplete }) => {
   
   const openDoor = () => {
     if(hasKeyOne) {
       setDoorOneOpen(true)
       console.log('you opened the door!!')
     }
+  
   }
 
-  return doorOneOpen ? (
-    <img src={doorOneOpenImg} className='door'/>
+  const levelComplete = () => {
+    if(doorOneOpen) {
+      setLevelOneComplete(true)
+      console.log('Level complete!')
+    }
+}
+
+return doorOneOpen ? (
+    <img src={doorOneOpenImg} className='door' onClick={levelComplete}/>
   ) : ( <img src={doorOneClosedImg} onClick={openDoor} className='door'/> )
 }
