303a98db709aa1de811eeea0d28fa3d7921cd25e
arhussain1 <a.r.hussain@hotmail.com>
2022-05-09 15:05:24 +0100

    Created bowl and spikeGuarding components
    Co-authored-by: Christian Pinna <ChrisPinna@users.noreply.github.com>

---
+++ src/components/levels/LevelThree.js
@@ -0,0 +1,12 @@
+
+
+import { useContext, useState, useEffect } from 'react'
+import { AppContext } from '../../AppContext'
+
+export const LevelThree = () => {
+  return (
+    <div className ="level-three">
+      
+    </div>
+  )
+}
