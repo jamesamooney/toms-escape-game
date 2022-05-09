b647a34ba288d23980edc00ebbd78752b1324b9c
James Mooney <jmooney9304@gmail.com>
2022-05-09 22:20:18 +0100

    missing context for playbutton

--- src/App.js
+++ src/App.js
@@ -89,12 +89,8 @@ function App() {
   
   return (
     <div className= "whole-screen">
-<<<<<<< HEAD
-      <AppContext.Provider value={{pouch, setPouch, playerLocation, setPlayerLocation, logs, setLogs, hasBook, setHasBook, isPadlockSolved, setPadlockSolved, doorOneOpen, setDoorOneOpen, hasBroom, setHasBroom, hasTurnedOnPower, setHasTurnedOnPower, light1, setLight1, light2, setLight2, light3, setLight3, isSafeSolved, setSafeSolved, hasPaper1, setHasPaper1, hasPaper2, setHasPaper2, hasPaper3, setHasPaper3, hasPaper4, setHasPaper4, hasPaper5, setHasPaper5, hasRock, setHasRock, safeAppears, setSafeAppears, isSafeClicked, setSafeClicked, isWindowBroken, setIsWindowBroken, savedMinutes, setSavedMinutes, savedSeconds, setSavedSeconds, setFinalTime, finalTime, hasShovel, setHasShovel, showPlayButton, setShowPlayButton, radioPlaying, setRadioPlaying}} >
-=======
 
-      <AppContext.Provider value={{userName, setUserName, pouch, setPouch, playerLocation, setPlayerLocation, logs, setLogs, hasBook, setHasBook, isPadlockSolved, setPadlockSolved, doorOneOpen, setDoorOneOpen, hasBroom, setHasBroom, hasTurnedOnPower, setHasTurnedOnPower, light1, setLight1, light2, setLight2, light3, setLight3, isSafeSolved, setSafeSolved, hasPaper1, setHasPaper1, hasPaper2, setHasPaper2, hasPaper3, setHasPaper3, hasPaper4, setHasPaper4, hasPaper5, setHasPaper5, hasRock, setHasRock, safeAppears, setSafeAppears, isSafeClicked, setSafeClicked, isWindowBroken, setIsWindowBroken, savedMinutes, setSavedMinutes, savedSeconds, setSavedSeconds, setFinalTime, finalTime, hasShovel, setHasShovel, chestUnlocked, setChestUnlocked, chestOpen, setChestOpen, hasDetonator, setHasDetonator, hasTnt, setHasTnt, hasChestKey, setHasChestKey}} >
->>>>>>> main
+      <AppContext.Provider value={{userName, setUserName, pouch, setPouch, playerLocation, setPlayerLocation, logs, setLogs, hasBook, setHasBook, isPadlockSolved, setPadlockSolved, doorOneOpen, setDoorOneOpen, hasBroom, setHasBroom, hasTurnedOnPower, setHasTurnedOnPower, light1, setLight1, light2, setLight2, light3, setLight3, isSafeSolved, setSafeSolved, hasPaper1, setHasPaper1, hasPaper2, setHasPaper2, hasPaper3, setHasPaper3, hasPaper4, setHasPaper4, hasPaper5, setHasPaper5, hasRock, setHasRock, safeAppears, setSafeAppears, isSafeClicked, setSafeClicked, isWindowBroken, setIsWindowBroken, savedMinutes, setSavedMinutes, savedSeconds, setSavedSeconds, setFinalTime, finalTime, hasShovel, setHasShovel, chestUnlocked, setChestUnlocked, chestOpen, setChestOpen, hasDetonator, setHasDetonator, hasTnt, setHasTnt, hasChestKey, setHasChestKey, radioPlaying, setRadioPlaying, showPlayButton, setShowPlayButton}} >
         <div className='game-window'>
           <div className='game-background'>
             <MyStopwatch />
