a84cb7540fe5368c5ade66b15859cf658636a152
James Mooney <jmooney9304@gmail.com>
2022-05-09 15:47:32 +0100

    Audio auto play not working

--- src/components/levels/LevelZero.js
+++ src/components/levels/LevelZero.js
@@ -1,6 +1,8 @@
 import { PlayButton } from '../level-items/PlayButton'
 import themeTune from '../audio/AppThemeTune.wav'
 import { useEffect } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+import { Sound } from 'react';
 
 export const LevelZero = () => {
 
@@ -8,14 +10,19 @@ export const LevelZero = () => {
     new Audio(themeTune).play();
   },[])
 
+  // const playAudio = () => {
+  //   new Audio(themeTune).play();
+  // }
+  // useEffect(
+  //   () => { playAudio() }, []);
+
 
   return (
     <div className='level-zero'>
 
       <PlayButton />
-  
-
-
+   
+      
 
     </div>
   )
