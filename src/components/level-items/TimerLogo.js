80139b470909f52b5fe7d26fd1d16ef03d7ab093
James Mooney <jmooney9304@gmail.com>
2022-05-10 10:58:37 +0100

    timer moved

---
+++ src/components/level-items/TimerLogo.js
@@ -0,0 +1,12 @@
+import timerLogo from "../../images/items/timer-logo.png"
+
+export const TimerLogo = () => {
+
+  return (
+    <div>
+      <img src={timerLogo}
+        id="timer-logo"
+      />
+    </div>
+  )
+}
