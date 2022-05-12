c9e4e38468afb0baedc857b4be73e25bb977cd97
Nicola Cortese <nicolacortese92@gmail.com>
2022-05-12 14:52:28 +0100

    added scripts for heroku build

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ app.use(cors());
 app.use("/scores", scoresRouter);
 
 
-const CONNECTION_URL = 'mongodb+srv://Abdul:Makers123@cluster0.gln4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.MONGODB_URI || 'mongodb+srv://Abdul:Makers123@cluster0.gln4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 3030;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .then(() => {
+    app.use(express.static('client/build'));
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
   .catch((error) => console.log(error.message))
   
